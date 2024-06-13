76d208c76c3b00acc4f6a42b27e43f9dd78fd8f9
mkheck13 <mkheck13@gmail.com>
2024-06-13 12:05:58 -0700

    bryan test

---
+++ scripts/ap.js
@@ -0,0 +1,10 @@
+// want a function the counts from one to ten
+
+// made a for loop that starts at 1, we need it to go to ten so we set the loop length at 11, then it will iterate in the console until it reaches 10
+
+function bryanTest(){
+    for (let i = 1; i < 11; i++ ){
+        console.log(i)
+    }
+}
+bryanTest();
